f8a528b96daa316052348913f77377b7cbc36d55
Patricio Córdova <patricio@cs.toronto.edu>
2015-08-21 13:35:05 -0400

    fonts testing

--- js/viz.js
+++ js/viz.js
@@ -1,49 +1,68 @@
-(function(){
+(function() {
 
-var width = Math.max(960, innerWidth),
-    height = Math.max(500, innerHeight);
+    var width = Math.max(960, innerWidth),
+        height = Math.max(500, innerHeight);
 
-var i = 0;
+    var i = 0;
 
-var svg = d3.select("#hidden").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var svg = d3.select("#hidden").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-var rect = svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .on("ontouchstart" in document ? "touchmove" : "mousemove", particle)
-    .on("click", background);
+    var rect = svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .on("ontouchstart" in document ? "touchmove" : "mousemove", particle)
+        .on("click", background);
 
-function particle() {
-  var m = d3.mouse(this);
+    function particle() {
+        var m = d3.mouse(this);
 
-  svg.insert("circle", "rect")
-      .attr("cx", m[0])
-      .attr("cy", m[1])
-      .attr("r", 10)
-      .style("fill", d3.hsl((i = (i + 1) % 360), 1, .5))
-      .style("stroke", d3.hsl((i), 1, .5))
-      .style("stroke-opacity", 1)
-      .style("fill-opacity", 1)
-    .transition()
-      .duration(20000)
-      .ease(Math.sqrt)
-      .attr("r", 50)
-      .style("fill-opacity", 1e-6)
-      .style("stroke-opacity", 1e-6)
-      .remove();
+        svg.insert("circle", "rect")
+            .attr("cx", m[0])
+            .attr("cy", m[1])
+            .attr("r", 10)
+            .style("fill", d3.hsl((i = (i + 1) % 360), 1, .5))
+            .style("stroke", d3.hsl((i), 1, .5))
+            .style("stroke-opacity", 1)
+            .style("fill-opacity", 1)
+            .transition()
+            .duration(20000)
+            .ease(Math.sqrt)
+            .attr("r", 50)
+            .style("fill-opacity", 1e-6)
+            .style("stroke-opacity", 1e-6)
+            .remove();
 
-  d3.event.preventDefault();
-}
+        d3.event.preventDefault();
+    }
 
-function background(){
-  rect.style("fill", d3.hsl((i), 1, .5))
-      .style("fill-opacity", 1)
-    .transition()
-      .duration(10000)
-      .style("fill", "#FFFFFF")
-      .style("fill-opacity", 1e-6);
-}
+    function background() {
+        rect.style("fill", d3.hsl((i), 1, .5))
+            .style("fill-opacity", 1)
+            .transition()
+            .duration(10000)
+            .style("fill", "#FFFFFF")
+            .style("fill-opacity", 1e-6);
+    }
+
+    function trans(j) {
+        rect.style("fill", d3.hsl((j = (j + 10) % 360), 1, .5))
+            .style("fill-opacity", 1)
+          .transition()
+            .duration(100)
+            .style("fill", "#FFFFFF")
+            .style("fill-opacity", 1e-6)
+          .each(function() {
+              trans(j);
+          });
+
+    }
+
+    $(document).ready(function() {
+    var j = 0;
+    trans(j);
+  });
+
 
 })(window);
